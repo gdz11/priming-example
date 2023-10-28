78b5ed18197023f2fa85da0ef0632b6f3e6e74ba
gio <mail@example.com>
2023-10-28 11:25:46 +0400

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from './photo-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeng-example';
+
+  images: any[] | undefined;
+    
+  responsiveOptions: any[] | undefined;
+
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit() {
+      this.photoService.getImages().then((images) => (this.images = images));
+      this.responsiveOptions = [
+          {
+              breakpoint: '1024px',
+              numVisible: 5
+          },
+          {
+              breakpoint: '768px',
+              numVisible: 3
+          },
+          {
+              breakpoint: '560px',
+              numVisible: 1
+          }
+      ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleriaModule } from 'primeng/galleria';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    GalleriaModule,
     AppRoutingModule
   ],
   providers: [],
